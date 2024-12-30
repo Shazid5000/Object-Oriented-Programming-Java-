3bd9a6efb4f472c9af1b24a2f24f9d1c68ceee04
Shazidul Alam <143749400+Shazid5000@users.noreply.github.com>
2024-12-30 13:24:09 +0600

    Add files via upload

---
+++ 12-01-2023/Date12thJan2023homework/src/main/java/com/mycompany/date12thjan2023homework/Date12thJan2023homework.java
@@ -0,0 +1,6 @@
+package com.mycompany.date12thjan2023homework;
+public class Date12thJan2023homework {
+    public static void main(String[] args) {
+    int x=9, y=2, z=7;
+    System.out.println("the value of x,y,z is"+x+","+y+","+z);
+    }}
